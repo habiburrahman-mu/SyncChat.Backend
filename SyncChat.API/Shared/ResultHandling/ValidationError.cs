80a7cbcb2559f86419039ac7f927124cceb21eb6
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-05-13 01:29:15 +0600

    feat(result-handling): add ValidationError record type
    Introduces a new `ValidationError` record in the `SyncChat.API.Shared.ResultHandling` namespace. This record extends the `Error` class and holds an array of `Error` objects. It features a constructor for initialization and a static method `FromResults` to create instances from a collection of `Result` objects, specifically filtering for failures.

---
+++ SyncChat.API/Shared/ResultHandling/ValidationError.cs
@@ -0,0 +1,18 @@
+﻿namespace SyncChat.API.Shared.ResultHandling;
+
+public record ValidationError : Error
+{
+    private readonly Error[] Errors;
+
+    public ValidationError(Error[] errors)
+        : base(
+            "Validation.General",
+            "One or more validation",
+            ErrorType.Validation)
+    {
+        this.Errors = errors;
+    }
+
+    public static ValidationError FromResults(IEnumerable<Result> results) => 
+        new(results.Where(r => r.IsFailure).Select(r => r.Error).ToArray());
+}
