b49daefb70f176f3ae89f41edea123842c5f0374
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-06-19 00:49:50 +0600

    feat(persistence): add ConversationMemberConfiguration class
    Introduces the ConversationMemberConfiguration class to configure the ConversationMember entity in Entity Framework Core. This includes setting the primary key, defining the Settings property as jsonb, creating a unique index on ConversationId and UserId, and establishing foreign key relationships with cascading delete behavior for Conversation and User entities.

---
+++ SyncChat.API/Infrastructure/Persistence/Configurations/ConversationMemberConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SyncChat.API.Shared.Entities;
+
+namespace SyncChat.API.Infrastructure.Persistence.Configurations;
+
+public class ConversationMemberConfiguration : IEntityTypeConfiguration<ConversationMember>
+{
+    public void Configure(EntityTypeBuilder<ConversationMember> builder)
+    {
+        builder.HasKey(cm => cm.MemberId);
+
+        builder.Property(x => x.Settings)
+            .HasColumnType("jsonb")
+            .HasDefaultValueSql("'{}'::jsonb");
+
+        builder.Property(x => x.Role)
+            .HasConversion<string>();
+
+        builder.HasIndex(cm => new { cm.ConversationId, cm.UserId })
+            .IsUnique();
+
+        builder.HasOne(cm => cm.Conversation)
+            .WithMany(c => c.Members)
+            .HasForeignKey(cm => cm.ConversationId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(cm => cm.User)
+            .WithMany()
+            .HasForeignKey(cm => cm.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
