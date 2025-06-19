db54b4abc9b201a8f950409b31defcbfb608762e
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-06-19 23:55:38 +0600

    feat(persistence): add MessageStatusConfiguration class
    This commit introduces the `MessageStatusConfiguration` class that implements the `IEntityTypeConfiguration<MessageStatus>` interface. It configures the `MessageStatus` entity in the Entity Framework Core context, defining the primary key, property conversions, unique indexes, and relationships with `Message` and `User`, including their delete behaviors.

---
+++ SyncChat.API/Infrastructure/Persistence/Configurations/MessageStatusConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SyncChat.API.Shared.Entities;
+
+namespace SyncChat.API.Infrastructure.Persistence.Configurations;
+
+public class MessageStatusConfiguration : IEntityTypeConfiguration<MessageStatus>
+{
+    public void Configure(EntityTypeBuilder<MessageStatus> builder)
+    {
+        builder.HasKey(x => x.StatusId);
+
+        builder.Property(x => x.Status).HasConversion<string>();
+
+        builder.HasIndex(x => new { x.MessageId, x.UserId }).IsUnique();
+
+        builder.HasOne(ms => ms.Message)
+               .WithMany(m => m.Statuses)
+               .HasForeignKey(ms => ms.MessageId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(ms => ms.User)
+               .WithMany()
+               .HasForeignKey(ms => ms.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
