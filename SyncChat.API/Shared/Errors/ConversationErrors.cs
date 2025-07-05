be52a6a4af9a67b6c72242438aa7bfc27d8995e0
Habibur Rahman <habiburrahman.mu@gmail.com>
2025-07-05 11:29:50 +0600

    feat(errors): add ConversationErrors class for validation
    Introduces a new static class `ConversationErrors` in the `SyncChat.API.Shared.Errors` namespace. This class features a method `InvalidUser` that validates user IDs and returns an appropriate error message when the ID is invalid.

---
+++ SyncChat.API/Shared/Errors/ConversationErrors.cs
@@ -0,0 +1,10 @@
+﻿using SyncChat.API.Shared.ResultHandling;
+
+namespace SyncChat.API.Shared.Errors;
+
+public static class ConversationErrors
+{
+    public static Error InvalidUser(long userId) => Error.Validation(
+        "Conversation.InvalidUser",
+        $"The user with the Id = '{userId}' is invalid.");
+}
