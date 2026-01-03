1776c0fce573c4dbbf49f7752788bd4ca59b0eb7
Habibur Rahman <habiburrahman.mu@gmail.com>
2026-01-03 12:16:31 +0600

    feat: add configuration for user auth provider

---
+++ SyncChat.API/Infrastructure/Persistence/Configurations/UserAuthProviderConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SyncChat.API.Shared.Entities;
+
+namespace SyncChat.API.Infrastructure.Persistence.Configurations;
+
+public sealed class UserAuthProviderConfiguration : IEntityTypeConfiguration<UserAuthProvider>
+{
+    public void Configure(EntityTypeBuilder<UserAuthProvider> builder)
+    {
+        builder.HasKey(uap => uap.Id);
+
+        builder.Property(uap => uap.Provider)
+            .HasConversion<int>()
+            .IsRequired();
+
+        builder.Property(uap => uap.ProviderUserId)
+            .IsRequired()
+            .HasMaxLength(256);
+
+        builder.Property(uap => uap.Email)
+            .HasMaxLength(256);
+
+       // Relationship
+       builder.HasOne(uap => uap.User)
+            .WithMany(u => u.UserAuthProviders)
+            .HasForeignKey(uap => uap.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Unique (Provider, ProviderUserId)
+        builder.HasIndex(uap => new { uap.Provider, uap.ProviderUserId })
+            .IsUnique();
+
+        // Unique (Provider, Email) where Email is not null
+        builder.HasIndex(uap => new { uap.Provider, uap.Email })
+            .IsUnique()
+            .HasFilter("\"Email\" IS NOT NULL");
+    }
+}
