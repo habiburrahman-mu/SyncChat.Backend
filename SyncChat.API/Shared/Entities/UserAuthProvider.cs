72a4b54687a3204c5e7931149ca405cb6261fa44
Habibur Rahman <habiburrahman.mu@gmail.com>
2026-01-03 12:07:10 +0600

    feat: add user auth provider entity

---
+++ SyncChat.API/Shared/Entities/UserAuthProvider.cs
@@ -0,0 +1,24 @@
+﻿namespace SyncChat.API.Shared.Entities;
+
+public class UserAuthProvider
+{
+    public long Id { get; set; }
+    public long UserId { get; set; }
+    public AuthProvider Provider { get; set; }
+    public string ProviderUserId { get; set; } = null!;
+    public string? Email { get; set; }
+    public DateTimeOffset LinkedAt { get; set; } = DateTimeOffset.UtcNow;
+
+    // Navigation
+    public User User { get; set; } = null!;
+}
+
+public enum AuthProvider
+{
+    Local = 1,
+    Google = 2,
+    Facebook = 3,
+    Twitter = 4,
+    GitHub = 5,
+    Microsoft = 6
+}
