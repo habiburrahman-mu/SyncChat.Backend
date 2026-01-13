061b37b5dac8b34389f029a98f57594fd5f00dd8
Habibur Rahman <habiburrahman.mu@gmail.com>
2026-01-13 21:52:52 +0600

    feat: add settings for minio storage

--- SyncChat.API/Program.cs
+++ SyncChat.API/Program.cs
@@ -22,13 +22,13 @@ builder.Services.Configure<JWTSettings>(jwtSection);
 JWTSettings jwtSettingsInstance = jwtSection.Get<JWTSettings>()!;
 IOptions<JWTSettings> jwtOptions = Options.Create(jwtSettingsInstance);
 
-//IConfigurationSection googleAuthSection = builder.Configuration.GetSection("GoogleAuth");
-//builder.Services.Configure<GoogleAuthSettings>(googleAuthSection);
-//GoogleAuthSettings googleAuthSettingsInstance = googleAuthSection.Get<GoogleAuthSettings>()!; // TODO: Null Check?
-//IOptions<GoogleAuthSettings> googleAuthOptions = Options.Create(googleAuthSettingsInstance);
-
 builder.Services.AddOptions<GoogleAuthSettings>()
     .Bind(builder.Configuration.GetSection("GoogleAuth"))
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
+
+builder.Services.AddOptions<StorageSettings>()
+    .Bind(builder.Configuration.GetSection("Storage"))
     .ValidateDataAnnotations()
     .ValidateOnStart();
 
---
+++ SyncChat.API/Shared/Configuration/StorageSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace SyncChat.API.Shared.Configuration;
+
+public sealed class StorageSettings
+{
+    public string Endpoint { get; init; } = default!;
+    public int Port { get; init; }
+    public string AccessKey { get; init; } = default!;
+    public string SecretKey { get; init; } = default!;
+    public string Bucket { get; init; } = default!;
+    public bool UseSSL { get; init; }
+}
