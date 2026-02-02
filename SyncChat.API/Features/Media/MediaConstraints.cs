bc9d3048233aad25344b64bdbc5319716a33798b
Habibur Rahman <habiburrahman.mu@gmail.com>
2026-02-02 09:17:25 +0600

    feat: add media constraint for future validations

---
+++ SyncChat.API/Features/Media/MediaConstraints.cs
@@ -0,0 +1,17 @@
+﻿namespace SyncChat.API.Features.Media;
+
+public static class MediaConstraints
+{
+    public static readonly HashSet<string> AllowedImageMimeTypes = new(StringComparer.OrdinalIgnoreCase)
+    {
+        "image/jpeg",
+        "image/png",
+        "image/gif",
+        "image/webp",
+        "image/svg+xml",
+        "image/bmp",
+        "image/tiff"
+    };
+
+    public const long MaxImageFileSizeBytes = 5 * 1024 * 1024; // 5 MB
+}
