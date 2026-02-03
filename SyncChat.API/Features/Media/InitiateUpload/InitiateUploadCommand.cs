e264e6c0b4b6f4863f50b06ba4879d85019164e5
Habibur Rahman <habiburrahman.mu@gmail.com>
2026-02-03 22:31:48 +0600

    feat: add validator for initiate upload media command

--- SyncChat.API/Features/Media/InitiateUpload/InitiateUploadCommand.cs
+++ SyncChat.API/Features/Media/InitiateUpload/InitiateUploadCommand.cs
@@ -1,4 +1,6 @@
-﻿using SyncChat.API.Infrastructure.Persistence;
+﻿using FluentValidation;
+using SyncChat.API.Infrastructure.Persistence;
+using SyncChat.API.Shared.Errors;
 using SyncChat.API.Shared.ResultHandling;
 using SyncChat.API.Shared.Sender.Contracts;
 using SyncChat.API.Shared.Storage.Contracts;
@@ -23,5 +25,29 @@ public sealed class InitiateUploadCommandHandler : ICommandHandler<InitiateUploa
     public Task<Result<InitiateUploadResponse>> HandleAsync(InitiateUploadCommand request, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
+    }
+}
+
+public sealed class InitiateUploadCommandValidator : AbstractValidator<InitiateUploadCommand>
+{
+    public InitiateUploadCommandValidator()
+    {
+        RuleFor(x => x.Owner.Id)
+            .NotEmpty().WithMessage("Owner ID must not be empty.");
+
+        RuleFor(x => x.Owner.Type)
+            .IsInEnum().WithMessage("Owner Type must be valid.");
+
+        RuleFor(x => x.File.MimeType)
+            .NotEmpty()
+                .WithMessage("File MimeType must not be empty.")
+            .Must(MediaConstraints.IsMimeTypeAllowed)
+                .WithMessage(x => MediaErrors.UnsupportedMimeType(x.File.MimeType).Description);
+
+        RuleFor(x => x.File.SizeBytes)
+            .NotEmpty().WithMessage("File SizeBytes must not be empty.")
+            .GreaterThan(0).WithMessage("File SizeBytes must be greater than zero.")
+            .LessThanOrEqualTo(MediaConstraints.MaxImageFileSizeBytes)
+                .WithMessage(x => MediaErrors.FileTooLarge(MediaConstraints.MaxImageFileSizeBytes).Description);
     }
 }
