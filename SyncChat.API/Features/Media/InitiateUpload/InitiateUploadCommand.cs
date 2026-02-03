518200fcd59647b17b99e57fe1bb2277e9d7801b
Habibur Rahman <habiburrahman.mu@gmail.com>
2026-02-03 22:19:50 +0600

    feat: update initiate upload command

--- SyncChat.API/Features/Media/InitiateUpload/InitiateUploadCommand.cs
+++ SyncChat.API/Features/Media/InitiateUpload/InitiateUploadCommand.cs
@@ -1,7 +1,27 @@
-﻿using SyncChat.API.Shared.Sender.Contracts;
+﻿using SyncChat.API.Infrastructure.Persistence;
+using SyncChat.API.Shared.ResultHandling;
+using SyncChat.API.Shared.Sender.Contracts;
+using SyncChat.API.Shared.Storage.Contracts;
 
 namespace SyncChat.API.Features.Media.InitiateUpload;
 
 public sealed record InitiateUploadCommand(
     MediaOwnerDto Owner,
-    MediaFileDescriptorDto File) : ICommand;
+    MediaFileDescriptorDto File) : ICommand<InitiateUploadResponse>;
+
+public sealed class InitiateUploadCommandHandler : ICommandHandler<InitiateUploadCommand, InitiateUploadResponse>
+{
+    private readonly ApplicationDbContext dbContext;
+    private readonly IBlobStorage blobStorage;
+
+    public InitiateUploadCommandHandler(ApplicationDbContext dbContext, IBlobStorage blobStorage)
+    {
+        this.dbContext = dbContext;
+        this.blobStorage = blobStorage;
+    }
+
+    public Task<Result<InitiateUploadResponse>> HandleAsync(InitiateUploadCommand request, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ SyncChat.API/Features/Media/InitiateUpload/InitiateUploadResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace SyncChat.API.Features.Media.InitiateUpload;
+
+public sealed record InitiateUploadResponse(
+    Guid MediaId,
+    Uri UploadUri,
+    DateTimeOffset Expiration);
