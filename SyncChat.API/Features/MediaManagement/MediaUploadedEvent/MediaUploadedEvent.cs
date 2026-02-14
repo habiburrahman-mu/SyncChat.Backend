4eef04f79e668f469a328e6abdec24839cdf2256
Habibur Rahman <habiburrahman.mu@gmail.com>
2026-02-14 12:18:23 +0600

    feat: add media uploaded event and handler

---
+++ SyncChat.API/Features/MediaManagement/MediaUploadedEvent/MediaUploadedEvent.cs
@@ -0,0 +1,8 @@
+﻿using SyncChat.API.Shared.Events;
+
+namespace SyncChat.API.Features.MediaManagement.MediaUploadedEvent;
+
+public sealed record class MediaUploadedEvent(Guid MediaId) : IDomainEvent
+{
+    public DateTimeOffset OccurredAt { get; } = DateTimeOffset.UtcNow;
+}
---
+++ SyncChat.API/Features/MediaManagement/MediaUploadedEvent/MediaUploadedEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SyncChat.API.Infrastructure.Persistence;
+using SyncChat.API.Shared.Entities;
+using SyncChat.API.Shared.Events;
+using SyncChat.API.Shared.Storage.Contracts;
+
+namespace SyncChat.API.Features.MediaManagement.MediaUploadedEvent;
+
+public sealed class MediaUploadedEventHandler : IDomainEventHandler<MediaUploadedEvent>
+{
+    private readonly ApplicationDbContext dbContext;
+    private readonly IBlobStorage blobStorage;
+
+    public MediaUploadedEventHandler(ApplicationDbContext dbContext, IBlobStorage blobStorage)
+    {
+        this.dbContext = dbContext;
+        this.blobStorage = blobStorage;
+    }
+
+    public async Task HandleAsync(MediaUploadedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        Media? media = await dbContext.Media.FirstOrDefaultAsync(x => x.Id == domainEvent.MediaId, cancellationToken);
+
+        if (media is null || media.State != MediaState.Uploaded) return;
+
+        // This block is done for future works like validate blob, generate thumbnails, mark active.
+    }
+}
